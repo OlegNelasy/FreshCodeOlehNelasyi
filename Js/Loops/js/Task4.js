773ae4220c5094b37694cd05373ee9722d9be410
OlegNelasy <oleg.nelasy@gmail.com>
2026-07-29 19:02:22 +0300

    JS Loops: Refactoring code task 4
    JS Loops: Refactoring code task 4

--- Js/Loops/js/Task4.js
+++ Js/Loops/js/Task4.js
@@ -1,5 +1,5 @@
 const hiddenNumber = Math.floor(Math.random() * 20) + 1;
-console.log(`hiddenNumber = ${hiddenNumber}`);
+// console.log(`hiddenNumber = ${hiddenNumber}`);
 
 let inputNumber;
 
@@ -9,22 +9,21 @@ do {
     if(inputNumber === null){
         console.log(`Игра завершина вы нажали отмена.`);
         break;
-    } else {
-        inputNumber = Number(inputNumber);
-        if(Number.isNaN(inputNumber)){
-            console.log(`Ошибка!!! Введите цисло а не символ!`);
-            continue;
-        }
-        if((inputNumber > hiddenNumber + 3 || inputNumber < hiddenNumber - 3)){
-            console.log(`Холодно`);
-        } else {
-            if(hiddenNumber === inputNumber){
-                console.log(`Число ${hiddenNumber} отгаданно!`);
-                continue;
-            }
-            console.log(`Горяче`);
-        };
+    } 
+    
+    inputNumber = Number(inputNumber);
+    if(Number.isNaN(inputNumber)){
+        console.log(`Ошибка!!! Введите цисло а не символ!`);
+        continue;
+    }
 
+    if((inputNumber > hiddenNumber + 3 || inputNumber < hiddenNumber - 3)){
+        console.log(`Холодно`);
+    } else if(hiddenNumber === inputNumber){
+        console.log(`Число ${hiddenNumber} отгаданно!`);
+        continue;
+    } else {
+        console.log(`Горяче`);
     }
 } while (hiddenNumber !== inputNumber);
 
