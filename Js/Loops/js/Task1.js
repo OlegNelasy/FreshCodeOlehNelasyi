c635a2bfa9059bf6e387d1fd41f1f48724c9b933
OlegNelasy <oleg.nelasy@gmail.com>
2026-07-29 16:15:14 +0300

    Сompleted task 1
    Сompleted task 1

---
+++ Js/Loops/js/Task1.js
@@ -0,0 +1,18 @@
+console.log("============for=============");
+for(let i = 25; i >= 0; i--){
+    console.log(i);
+}
+
+console.log("============while============");
+let i = 25;
+while(i >= 0){
+    console.log(i);
+    i--;
+}
+
+console.log("============Do while=========");
+i = 25;
+do {
+  console.log(i);
+  i--;
+} while (i >= 0);
