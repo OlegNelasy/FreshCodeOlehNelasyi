eef8d2f4e92674d11965f4757f8a18477b6b4ecf
OlegNelasy <oleg.nelasy@gmail.com>
2026-07-29 18:54:14 +0300

    JS Loops: Сompleted task 5
    JS Loops: Сompleted task 5

---
+++ Js/Loops/js/Task5.js
@@ -0,0 +1,28 @@
+let inputNumber;
+let countSteps = 0;
+do {
+    inputNumber = prompt(`Введите количество шагов за день`);
+    
+    if(inputNumber === null || inputNumber === 'стоп'){
+        console.log(`Подщет окнчен общее количество шагов ${countSteps}`);
+        break;
+    }
+
+    inputNumber = Number(inputNumber);
+    if(Number.isNaN(inputNumber)){
+        console.log(`Ошибка!!! Введите цисло а не символ!`);
+        continue;
+    }
+
+    if(inputNumber < 0){
+        console.log(`Ошибка!!! Число не может быть отрицательным!`);
+        continue;
+    }
+
+    countSteps += inputNumber;
+
+} while(true);
+
+
+
+
