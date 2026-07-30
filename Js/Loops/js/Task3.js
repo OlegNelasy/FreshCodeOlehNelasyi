3a673ad70e136cadcf52e26d124d7900097bc24b
OlegNelasy <oleg.nelasy@gmail.com>
2026-07-30 21:30:49 +0300

    JS Loops: Refactoring code task 3
    JS Loops: Refactoring code task 3

--- Js/Loops/js/Task3.js
+++ Js/Loops/js/Task3.js
@@ -6,49 +6,30 @@ if(Number.isNaN(number1) || Number.isNaN(number2)){
 } else {
     let sum = 0;
     let buffNumber = 0;
-    if(number1 >= number2){
-        for (let i = number2; i <= number1; i++) {
-            sum += i;
-        }
-        console.log(`for: Сумма чисел в диапазоне от ${number2} до ${number1} = ${sum}`);
 
-        sum = 0
-        buffNumber = number2;
-        while(buffNumber <= number1){
-            sum += buffNumber;
-            buffNumber++
-        }
-        console.log(`while: Сумма чисел в диапазоне от ${number2} до ${number1} = ${sum}`);
-
-        sum = 0
-        buffNumber = number2;
-        do {
-            sum += buffNumber;
-            buffNumber++
-        } while (buffNumber <= number1);
-        console.log(`do while: Сумма чисел в диапазоне от ${number2} до ${number1} = ${sum}`);
-    } else {
-        for (let i = number1; i <= number2; i++) {
-            sum += i;
-        }
-        console.log(`for: Сумма чисел в диапазоне от ${number1} до ${number2} = ${sum}`);
-
-        sum = 0
-        buffNumber = number1;
-        while(buffNumber <= number2){
-            sum += buffNumber;
-            buffNumber++
-        }
-        console.log(`while: Сумма чисел в диапазоне от ${number1} до ${number2} = ${sum}`);
-
-        sum = 0
-        buffNumber = number1;
-        do {
-            sum += buffNumber;
-            buffNumber++
-        } while (buffNumber <= number2);
-        console.log(`do while: Сумма чисел в диапазоне от ${number1} до ${number2} = ${sum}`);
+    const min = Math.min(number1, number2);
+    const max = Math.max(number1, number2);
+    
+    for (let i = min; i <= max; i++) {
+        sum += i;
     }
+    console.log(`for: Сумма чисел в диапазоне от ${min} до ${max} = ${sum}`);
+            
+    sum = 0
+    buffNumber = min;
+    while(buffNumber <= max){
+        sum += buffNumber;
+        buffNumber++
+    }
+    console.log(`while: Сумма чисел в диапазоне от ${min} до ${max} = ${sum}`);
+
+    sum = 0
+    buffNumber = min;
+    do {
+        sum += buffNumber;
+        buffNumber++
+    } while (buffNumber <= max);
+    console.log(`do while: Сумма чисел в диапазоне от ${min} до ${max} = ${sum}`);
 }
 
 
