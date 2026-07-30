f1548a6ed37ad7d322673eee567326ccea32d3a3
OlegNelasy <oleg.nelasy@gmail.com>
2026-07-30 19:36:57 +0300

    JS Fanctions: Сompleted calculatePaintBoxes() for task 3
    JS Fanctions: Сompleted calculatePaintBoxes() for task 3

--- Js/Functions/js/Task3.js
+++ Js/Functions/js/Task3.js
@@ -25,42 +25,17 @@ function calculateSurfaceArea(shapeType, param1,  param2 = 1) {
     return 0;
 }
 
-function calculatePaintBoxes(area, layersCount = 1){
+/**
+ * Вычисляет необходимое количество целых банок краски для покраски заданной площади.
+ * @param {number} area - Площадь поверхности.
+ * @param {number} [layersCount=1] - Количество слоев фарбування (по умолчанию 1).
+ * @returns {number} Количество целых банок краски или 0 при некорректных данных.
+ */
+function calculatePaintBoxes(area, layersCount = 1) {
+    if (typeof area !== 'number' || Number.isNaN(area) || area <= 0 ||
+        typeof layersCount !== 'number' || Number.isNaN(layersCount) || layersCount <= 0) {
+        return 0;
+    }
 
+    return Math.ceil(area * layersCount * 0.1);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /**
-//  * Проверяет, делится ли первое число на второе нацело (без остатка).
-//  * @param {number} number1 - Первое число (делимое).
-//  * @param {number} number2 - Второе число (делитель).
-//  * @returns {boolean} Возвращает true, если делится без остатка, и false в противном случае.
-//  */
-// const checkMultiplicity = function(number1, number2) {
-//     return number1 % number2 === 0;
-// };
-
-// const number1 = Number(prompt("Введите первое число"));
-// const number2 = Number(prompt("Введите второе число"));
-
-// if(Number.isNaN(number1) || Number.isNaN(number2) || number2 === 0){
-//     console.log(`Ошибка ввода!!! Недопустимое значение!`);
-// } else {
-//     console.log(checkMultiplicity(number1, number2) ? `Деление без остатка` : `Деление с остатком`);
-// }
-
