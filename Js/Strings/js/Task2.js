6112764881e92e9abf9fe645850db70ef8298c97
OlegNelasy <oleg.nelasy@gmail.com>
2026-08-07 21:30:38 +0300

    JS Strings: Refactor code task 2
    JS Strings: Refactor code task 2

--- Js/Strings/js/Task2.js
+++ Js/Strings/js/Task2.js
@@ -20,14 +20,20 @@ const cities = "Київ, Львів, Одеса, Харків";
 //     return newArray;
 // };
 
-const toArray = cities => cities.split(', ');
-// const newCitiesArray = toArray(cities).concat('Дніпро'); //concat в отличии от push возращает новый масив push дину масива поле изменения. 
-// newCitiesArray.push('Дніпро');
-// console.log(newCitiesArray);
 
-// const newCities = newCitiesArray.join(' - ');
-const newCities = toArray(cities).concat('Дніпро').join(' - '); //Если надо промежуточная версия с выводами то раскоментировать 24 26 28 строки.
+// // const newCitiesArray = toArray(cities).concat('Дніпро'); //concat в отличии от push возращает новый масив push дину масива поле изменения. 
+// // newCitiesArray.push('Дніпро');
+// // console.log(newCitiesArray);
+// // const newCities = newCitiesArray.join(' - ');
+
+// const toArray = cities => cities.split(', ');
+// const newCities = toArray(cities).concat('Дніпро').join(' - '); 
+// console.log(newCities);
+
+const newCities = cities.split(', ').concat('Дніпро').join(' - '); 
 console.log(newCities);
+
+console.log(cities.split(', ').concat('Дніпро').join(' - ')); 
 
 // ✅ 2. Трансформація даних
 // Перетворити заданий рядок міст "Київ, Львів, Одеса, Харків"  на масив, додати в кінець масиву місто "Дніпро", а потім зібрати назад у рядок, де міста розділені через дефіс із пробілами ("Київ - Львів - Одеса...").
