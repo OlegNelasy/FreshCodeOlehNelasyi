b2f60261aa5372275dce2acb811ecddb281be87c
OlegNelasy <oleg.nelasy@gmail.com>
2026-08-10 17:03:33 +0300

    JS Closure: Сompleted task 1
    JS Closure: Сompleted task 1

---
+++ Js/Closure/js/Task1.js
@@ -0,0 +1,22 @@
+function customCounter(StartN , step) {
+  let n = StartN;
+
+  return function() {
+    return n += step;
+  };
+}
+
+const myCounter = customCounter(1, 5); 
+
+console.log(myCounter()); 
+console.log(myCounter()); 
+console.log(myCounter()); 
+
+
+// 1. ✅ Змініть приклад із заняття по замиканню (counter) так, щоб користувач задавав початкове значення лічильника (у прикладі із заняття це 0) і крок зміни лічильника (у прикладі із заняття це 1).
+
+// Приклади використання:
+// const myCounter = customCounter (1, 5);
+// const result1 = myCounter(); // => 6
+// const result2 = myCounter(); // => 11
+// const result3 = myCounter(); // => 16
