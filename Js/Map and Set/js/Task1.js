71e459b2b29a6cb6d123cf88059340b7056d76ae
OlegNelasy <oleg.nelasy@gmail.com>
2026-08-18 12:26:50 +0300

    JS Map and Set: Add function replaceNumbersWithWords() task 1
    JS Map and Set: Add function replaceNumbersWithWords() task 1

--- Js/Map and Set/js/Task1.js
+++ Js/Map and Set/js/Task1.js
@@ -23,6 +23,32 @@ console.log('Ищем по ключу 3', map1.get(3));
 console.log([...map1.keys()]);
 console.log([...map1.values()]);
 
+const text = 'This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents.';
+
+/**
+ * Заменяет числа в тексте на соответствующие им слова (числительные) из переданной коллекции Map.
+ * @param {string} str - Исходный текст, в котором нужно найти и заменить числа.
+ * @param {Map<number, string>} mapCollection - Коллекция Map, где ключ — это число, а значение — его текстовое представление.
+ * @returns {string} Новая строка, в которой числа заменены на слова.
+ */
+function replaceNumbersWithWords(str, mapCollection) {
+    const words = str.split(' ');
+
+    const translatedWords = words.map(word => {
+        const numberKey = Number(word);
+
+        if (mapCollection.has(numberKey)) {
+            return mapCollection.get(numberKey);
+        }
+
+        return word;
+    });
+
+    return translatedWords.join(' ');
+}
+
+console.log(replaceNumbersWithWords(text, map1));
+
 // 1.1. ✅ Нехай даний масив масивів ([[1,"first"], [3,"third"]]).
 // Створіть колекцію Map з цього масиву (по суті, вона співставляє значенням чисел їх порідковий числівник).
 // Отримайте кількість елементів.
@@ -30,3 +56,8 @@ console.log([...map1.values()]);
 // Здійсніть пошук за ключом.
 // Перевірити, чи є в мапі числівник для числа 2.
 // Отримайте список ключів та значень окремо.
+
+// 1.2. ✅ Написати функцію, яка перероблятиме текст з числами
+// "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents."
+//  на текст з порядковими числівниками за допомогою мапи вище:
+// "This year I will enter the first grade. I have two brothers. I am the third child of my parents."
