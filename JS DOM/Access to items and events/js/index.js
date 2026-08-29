dcfccbfe8bb0abd0fee2b47dd4651a91738c60a4
OlegNelasy <oleg.nelasy@gmail.com>
2026-08-29 16:20:15 +0300

    JS DOM Access to items and events: Сompleted task 3
    JS DOM Access to items and events: Сompleted task 3

--- JS DOM/Access to items and events/js/index.js
+++ JS DOM/Access to items and events/js/index.js
@@ -5,9 +5,23 @@
 // При натисканні на кнопку «Підписатися» її текст повинен змінюватися на «Ви підписані».
 // При повторному натисканні текст кнопки повинен повертатися до «Підписатися».
 const subscribeBtn = document.getElementById('subscribeBtn');
+subscribeBtn.textContent = 'Подписаться';
 
-subscribeBtn.addEventListener('click', () => {
-    const isActive = subscribeBtn.classList.toggle('btnActive');
-    subscribeBtn.textContent = isActive ? 'Вы подписаны' : 'Подписаться';
+subscribeBtn.addEventListener('click', (e) => {
+    const isActive = e.target.classList.toggle('btnActive');
+    e.target.textContent = isActive ? 'Вы подписаны' : 'Подписаться';
 });
-// console.log(subscribeBtn);
+
+// Реалізуйте кнопку з іконкою ❤️:
+// у початковому стані сердечко має бути сірим;
+// при натисканні воно повинно ставати червоним;
+// повторне натискання повинно повертати початковий стан.
+// Для зміни стану використовуйте JavaScript та CSS-класи.
+const likeBtn = document.querySelector('.btnLike');
+
+likeBtn.addEventListener('click', (e) => {
+    const isActive = e.target.classList.toggle('btnLikeActive');
+});
+
+
+// console.log(likeBtn);
