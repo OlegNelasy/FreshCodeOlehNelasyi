f98d321a7d42363309958655e1a8f13cac833639
OlegNelasy <oleg.nelasy@gmail.com>
2026-09-12 19:38:50 +0300

    JS DOM Asynchrony: Сompleted task 2
    JS DOM Asynchrony: Сompleted task 2

---
+++ JS DOM/Asynchrony/js/task2.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const apiUrl = 'https://catfact.ninja/fact';
+
+const catInfoContainer = document.createElement('div');
+const catInfoText = document.createElement('p');
+const btn = document.createElement('button');
+
+catInfoContainer.classList.add('cat-info-container');
+document.body.appendChild(catInfoContainer);
+
+
+catInfoText.classList.add('cat-info-text');
+catInfoContainer.appendChild(catInfoText);
+
+btn.classList.add('btn');
+btn.textContent = `Получить факт`
+catInfoContainer.appendChild(btn);
+
+
+// btn.addEventListener('click', () => {
+//             catInfoText.textContent = data.fact;
+//         })
+
+btn.addEventListener('click', () => {
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка при загрузке данных');
+            }
+            return response.json();
+        })
+        .then(data => {
+            //console.log("Полученные данные:", data);
+            //console.log("Сам факт:", data.fact);
+
+            catInfoText.textContent = data.fact;
+        })
+        .catch(error => {
+            console.error('Что-то пошло не так:', error);
+        });
+})
